7c75f9b171b2213083290e2c9a15bca002deceac
klodmone <kladmone54@gmail.com>
2023-10-14 19:23:09 +0200

    Summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,10 +17,17 @@ function createImgListMarkup(img) {
     })
     .join("");
 }
+
+function handleImageClick(event) {
+  event.preventDefault();
+  const target = event.target;
+  const largeImage = target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${largeImage}" width="800" height="600">
+  `);
+  instance.show();
+}
 const imgMarkup = createImgListMarkup(galleryItems);
 imagesList.insertAdjacentHTML("beforeend", imgMarkup);
-
-imagesList.addEventListener(){
-    
-};
+imagesList.addEventListener("click", handleImageClick);
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const imagesList = document.querySelector(".gallery");
+function createImgListMarkup(img) {
+  return img
+    .map(({ original, preview, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+function handleImageClick(event) {
+  event.preventDefault();
+  const target = event.target;
+  const largeImage = target.dataset.source;
+  const lightbox = new SimpleLightbox(".gallery a", {
+    largeImage,
+    animationSpeed: 250,
+  });
+}
+const imgMarkup = createImgListMarkup(galleryItems);
+imagesList.insertAdjacentHTML("beforeend", imgMarkup);
+imagesList.addEventListener("click", handleImageClick);
 
 console.log(galleryItems);
